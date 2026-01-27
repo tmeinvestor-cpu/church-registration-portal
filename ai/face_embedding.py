1769db64c1f1868a4831b88e84b6818c27ac1ccf
Jammy <tme.investor@gmail.com>
2026-01-27 14:13:27 +0100

    Delete ai directory

--- ai/face_embedding.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def get_embedding(face_object):
-    """
-    Converts InsightFace face → 512D vector
-    """
-    embedding = face_object.normed_embedding
-    return np.array(embedding).astype("float32")
