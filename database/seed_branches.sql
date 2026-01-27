da89439d05d1b8e6da86ffef623796fc9eb158ca
Jammy <tme.investor@gmail.com>
2026-01-27 13:21:31 +0100

    Create seed_branches.sql

---
+++ database/seed_branches.sql
@@ -0,0 +1,12 @@
+INSERT OR IGNORE INTO branches VALUES
+(1,'Nigeria','Abuja','COZA HQ – Guzape',1),
+(2,'Nigeria','Abuja','COZA Lugbe',1),
+
+(3,'Nigeria','Lagos','COZA Ikeja',1),
+(4,'Nigeria','Lagos','COZA VI',1),
+
+(5,'United Kingdom','London','COZA London',1),
+(6,'United Kingdom','Manchester','COZA Manchester',1),
+(7,'United Kingdom','Birmingham','COZA Birmingham',1),
+
+(8,'UAE','Dubai','COZA Dubai',1);
