e7133f18e70f9c7180affc9d8f54cb7368c6a8bc
Jammy <tme.investor@gmail.com>
2026-01-29 14:39:12 +0100

    Update location.js

--- portal/static/js/location.js
+++ portal/static/js/location.js
@@ -8,14 +8,23 @@ const BRANCHES = {
         "Lagos": [
             { id: 3, code: "COZA_LAGOS", name: "COZA Lagos", city: "Ikeja" },
             { id: 4, code: "COZA_VI/CHILDREN CHURCH", name: "COZA VI", city: "Maryland" }
+        ],
+        "Ilorin": [
+            { id: 5, code: "COZA_ILORIN", name: "COZA Ilorin", city: "Tanke" }
+        ],
+        "PH": [
+            { id: 6, code: "COZA_PH", name: "COZA PH", city: "Rumuomasi" }
         ]
     },
     "United Kingdom": {
         "Greater London": [
-            { id: 5, code: "COZA_LONDON", name: "COZA London", city: "London" }
+            { id: 7, code: "COZA_LONDON", name: "COZA London", city: "London" }
+        ],
+        "West Midlands": [
+            { id: 8, code: "COZA_BIRMINGHAM", name: "COZA Birmingham", city: "Birmingham" }
         ],
         "Greater Manchester": [
-            { id: 6, code: "COZA_MANCHESTER", name: "COZA Manchester", city: "Manchester" }
+            { id: 9, code: "COZA_MANCHESTER", name: "COZA Manchester", city: "Manchester" }
         ]
     }
 };
@@ -24,7 +33,7 @@ const BRANCHES = {
 function populateCountries() {
     const countrySelect = document.getElementById("country");
     countrySelect.innerHTML = '<option value="">Select Country</option>';
-    
+
     Object.keys(BRANCHES).forEach(country => {
         const option = document.createElement("option");
         option.value = country;
@@ -37,12 +46,12 @@ function populateCountries() {
 function populateStates(country) {
     const stateSelect = document.getElementById("state");
     stateSelect.innerHTML = '<option value="">Select State</option>';
-    
+
     const branchSelect = document.getElementById("branch_id");
     branchSelect.innerHTML = '<option value="">Select Branch</option>';
-    
+
     if (!country || !BRANCHES[country]) return;
-    
+
     Object.keys(BRANCHES[country]).forEach(state => {
         const option = document.createElement("option");
         option.value = state;
@@ -55,9 +64,9 @@ function populateStates(country) {
 function populateBranches(country, state) {
     const branchSelect = document.getElementById("branch_id");
     branchSelect.innerHTML = '<option value="">Select Branch</option>';
-    
+
     if (!country || !state || !BRANCHES[country] || !BRANCHES[country][state]) return;
-    
+
     BRANCHES[country][state].forEach(branch => {
         const option = document.createElement("option");
         option.value = branch.id;
@@ -69,12 +78,12 @@ function populateBranches(country, state) {
 // Initialize location dropdowns
 document.addEventListener("DOMContentLoaded", () => {
     populateCountries();
-    
+
     // Country change handler
     document.getElementById("country").addEventListener("change", (e) => {
         populateStates(e.target.value);
     });
-    
+
     // State change handler
     document.getElementById("state").addEventListener("change", (e) => {
         const country = document.getElementById("country").value;
